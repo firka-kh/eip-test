e4f716f2ae3353321b9215df80517725bdcf6ab7
firka-kh <fr.khokimov@gmail.com>
2026-03-18 10:29:22 +0500

    12
    qwe

--- 1/src/js/config/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-tailwind.config = {
-    theme: {
-        extend: {
-            fontFamily: { sans: ['Inter', 'sans-serif'], },
-            colors: { primary: '#5B4AF0', 'primary-hover': '#4A3BE0', background: '#F8F9FA', }
-        }
-    }
-};
